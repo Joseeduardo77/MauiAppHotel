a8d89a20f84214324a35d62efe5f0caeef852d75
Jose <dudujose.009@gmail.com>
2024-11-15 12:27:04 -0300

    Alteração

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,4 @@
+using MauiAppHotel.Models;
 using Microsoft.Maui.Controls;
 using System.Data;
 namespace MauiAppHotel.Views;
@@ -21,14 +22,32 @@ public partial class ContratacaoHospedagem : ContentPage
 
 	}
 
-	private void Button_Clicked(object sender, EventArgs e)
+	private async void Button_Clicked(object sender, EventArgs e)
 	{
 		try
 		{
-			Navigation.PushAsync(new HospedagemContratada());
+
+			Hospedagem h = new Hospedagem
+			{
+				 QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+				 QtdAdultos = Convert.ToInt32(stp_adultos.Value),
+                QtdCriancas = Convert.ToInt32(stp_criancas.Value),
+                QtdPetz = Convert.ToInt32(stp_petz.Value),
+				DataCheckIn = dtpck_checkin.Date,
+				DataCheckOut = dtpck_checkout.Date,
+            };
+
+
+
+			await Navigation.PushAsync(new HospedagemContratada()
+			{
+				BindingContext = h 
+			});
+
+
 		} catch (Exception ex)
 		{
-			DisplayAlert("Ops", ex.Message, "OK");
+			await DisplayAlert("Ops", ex.Message, "OK");
 		}
 
 	}
